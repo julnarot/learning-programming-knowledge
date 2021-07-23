8abe10d12172d4aa2b4806bb5313ee77c0a0e478
@Julnarot <rauljhonatan.tola@gmail.com>
2021-07-23 12:15:45 -0500

    baasic api delivery

---
+++ php/api-rest/api-delivery/src/application/controllers/Contact.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raul Jonatan  ( @julnarot )
+ * Date: 19/05/21
+ * Time: 16:41
+ */
+
+class Contact extends CI_Controller
+{
+public function index() {
+	echo("Contactos");
+}
+}
