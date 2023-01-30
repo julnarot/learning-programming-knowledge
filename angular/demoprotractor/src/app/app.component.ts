8a28b8a5b18e707f2ac943b44216cc4c44a27e5f
Raul Jonatan <rauljonatan.tolachoque@everis.nttdata.com>
2023-01-30 16:05:33 -0500

    crud app was implemented

--- angular/demoprotractor/src/app/app.component.ts
+++ angular/demoprotractor/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemService } from './item.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demoprotractor';
+  itemName  = "";
+  indexName  = -1;
+  items: any[] = [];
+  isEditing = false;
+
+  constructor(private itemService: ItemService) {
+  }
+  onSave() {
+    if(this.isEditing) {
+      this.itemService.editItem(this.indexName, this.itemName)
+    }else {
+      this.itemService.addItem(this.itemName)
+    }
+    this.restoreAndGetItems()
+  }
+  onSelectEdit(index) {
+    this.indexName = index;
+    this.itemName = this.items[index];
+    this.isEditing = true;
+  }
+  restoreAndGetItems() {
+    this.itemName = "";
+    this.indexName = -1;
+    this.isEditing = false;
+    this.onGetItems();
+  }
+  onGetItems() {
+    this.items = this.itemService.getItems();
+  }
+  onDelete(index) {
+    this.itemService.removeByIndex(index);
+    this.restoreAndGetItems();
+  }
+
 }
---
+++ angular/demoprotractor/src/app/item.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  items = [];
+  constructor() { }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item)
+  }
+
+  editItem(index, item) {
+    this.items[index] = item ;
+  }
+  removeByIndex(index){
+    this.items.splice(index, 1)
+  }
+  getByIndex(index) {
+    return this.items[index];
+  }
+}
