ced9422918e8edcf5d1b2efcaad86d1d88d59034
Raul Jonatan <rauljonatan.tolachoque@everis.nttdata.com>
2023-01-31 00:52:54 -0500

    e2e check input val

--- angular/demoprotractor/e2e/src/app.e2e-spec.ts
+++ angular/demoprotractor/e2e/src/app.e2e-spec.ts
@@ -21,3 +21,37 @@ describe('workspace-project App', () => {
     } as logging.Entry));
   });
 });
+
+describe('Adding new item', function() {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should add a todo', function() {
+    // browser.get('https://angularjs.org');
+
+    const firstItem = "firstItem";
+    page.navigateTo();
+
+    // element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+
+    page.getInputSave().sendKeys(firstItem);
+    // input.va
+    page.getButtonSave().click();
+    // console.log("jejeejJEEJEJ");
+    // console.log(page.getInputSaveValue())
+
+    expect(page.getInputSave().getAttribute('value')).toEqual(firstItem);
+
+    // var todoList = element.all(by.repeater('todo in todoList.todos'));
+    // expect(todoList.count()).toEqual(3);
+    // expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+    // // You wrote your first test, cross it off the list
+    // todoList.get(2).element(by.css('input')).click();
+    // var completedAmount = element.all(by.css('.done-true'));
+    // expect(completedAmount.count()).toEqual(2);
+  });
+});
