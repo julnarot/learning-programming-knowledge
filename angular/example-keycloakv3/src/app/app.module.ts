7aa9e1494b73d8f5496191afb8eccf9c34c1274d
@Julnarot <rauljhonatan.tola@gmail.com>
2023-06-01 16:22:49 -0500

    feat(new app): adding keycloak client with sso login

---
+++ angular/example-keycloakv3/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'master',
+        clientId: 'angularv2',
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        // checkLoginIframe: false,
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html',
+      },
+    });
+}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, KeycloakAngularModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+// set Valid redirect URIs value with simple [ * ]
+// http://localhost:8080/admin/master/console/#/master/realm-settings/security-defenses
+// [Content-Security-Policy] = frame-src 'self'; frame-ancestors 'self'; object-src 'none';
