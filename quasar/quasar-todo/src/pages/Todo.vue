0d235b3d903dd535e3edf7a2478103d201be617e
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-14 23:26:58 -0500

    feat(help): add help page and router related

---
+++ quasar/quasar-todo/src/pages/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page>
+    <h3>{{ title }}</h3>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/ExampleComponent.vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'TodoPage',
+  components: { ExampleComponent },
+  setup () {
+    const title = 'Todo';
+    return {  title };
+  }
+});
+</script>
