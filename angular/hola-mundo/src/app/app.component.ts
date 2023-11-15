7b53caa5edbdd4de9a6eb988ad7d1631577cf171
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-14 21:36:01 -0500

    feat(application): import person component

--- angular/hola-mundo/src/app/app.component.ts
+++ angular/hola-mundo/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PersonaComponent } from './persona/persona.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,PersonaComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
