ec3805823607d73d86f4ba7a293a63ed659b7d98
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-16 21:10:00 -0500

    practice02(angular): add reset method for personModel

--- angular/ntt-project02/src/app/app.component.ts
+++ angular/ntt-project02/src/app/app.component.ts
@@ -13,19 +13,15 @@ export class AppComponent implements OnInit {
 
   personList: PersonModel[] = [];
 
-  onList(): void {
-    console.log('hello Word');
-  }
-  nameChanged(nameInpuElement: Event) {
-    this.names = (<HTMLInputElement>nameInpuElement?.target).value;
-  }
-
   ngOnInit(): void {}
   onAddPerson() {
-    this.personList.push(
-      new PersonModel('Julnarot', this.names, 'Tola Choque', 3433445, [])
-    );
+    const newPerson = new PersonModel('S/N', this.names, 'S/N', undefined, []);
+    this.appendToList(newPerson);
+    newPerson.resetValues();
     this.resetForm();
+  }
+  appendToList(person: PersonModel) {
+    this.personList.push(person);
   }
   resetForm(): void {
     this.names = '';
--- angular/ntt-project02/src/app/model/person-model.ts
+++ angular/ntt-project02/src/app/model/person-model.ts
@@ -1,9 +1,17 @@
 export class PersonModel {
   constructor(
-    public user: string,
-    public names: string,
-    public surnames: string,
-    public dni: number,
-    public links: string[]
+    public user?: string,
+    public names?: string,
+    public surnames?: string,
+    public dni?: number,
+    public links?: string[]
   ) {}
+
+  resetValues(): void {
+    this.user = '';
+    this.names = '';
+    this.surnames = '';
+    this.dni = undefined;
+    this.links = [];
+  }
 }
