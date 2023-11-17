4690c98cfe526771987bc2aa909d523973eb1d7e
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-16 20:32:26 -0500

    practice(angular): use event change

--- angular/ntt-project02/src/app/app.component.ts
+++ angular/ntt-project02/src/app/app.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ntt-project02';
   enabled = true;
+  names = '';
+
+  onList(): void {
+    console.log('hello Word');
+  }
+  nameChanged(nameInpuElement: Event) {
+    this.names = (<HTMLInputElement>nameInpuElement?.target).value;
+  }
 }
