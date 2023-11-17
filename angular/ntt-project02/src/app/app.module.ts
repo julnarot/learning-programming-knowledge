a74d3b49322d8cd304ab15754ced957b8e745893
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-16 19:58:26 -0500

    feat(personComponent): add person component

--- angular/ntt-project02/src/app/app.module.ts
+++ angular/ntt-project02/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule
