52656fbeee8770688fa63eac39aade98ad686dc5
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-23 20:41:36 -0500

    angular(practice05): practice 05 was initilized, add base components

---
+++ angular/ntt-project05/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonAddComponent } from './components/person/person-add/person-add.component';
+import { PersonEditComponent } from './components/person/person-edit/person-edit.component';
+import { PersonListComponent } from './components/person/person-list/person-list.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonAddComponent,
+    PersonEditComponent,
+    PersonListComponent,
+    MenuComponent,
+    LoginComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/ntt-project05/src/app/entities/person-entity.ts
@@ -0,0 +1,4 @@
+export class PersonEntity {
+  id?: number;
+  name?: number;
+}
