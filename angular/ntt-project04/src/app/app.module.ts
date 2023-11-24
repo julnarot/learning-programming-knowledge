22200b01a22d2b5d0eff2b6513412ce49a60912e
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-23 19:58:51 -0500

    angular(practice04): app was initiliazed, form person

---
+++ angular/ntt-project04/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonFormComponent } from './components/person-form/person-form.component';
+import { PersonListComponent } from './components/person-list/person-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonFormComponent,
+    PersonListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
