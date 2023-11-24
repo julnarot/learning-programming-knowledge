8a8b1a3cf6d6ab8ecbd032cb3b0e1d6da3169bb2
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-23 20:19:57 -0500

    angular(practice04): added table

--- angular/ntt-project04/src/app/components/person-list/person-list.component.ts
+++ angular/ntt-project04/src/app/components/person-list/person-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { PersonModel } from 'src/app/models/person-model';
 
 @Component({
   selector: 'app-person-list',
   templateUrl: './person-list.component.html',
-  styleUrls: ['./person-list.component.css']
+  styleUrls: ['./person-list.component.css'],
 })
 export class PersonListComponent {
+  @Output() editingIndex= -1;
+  @Output() deletingIndex= -1;
+  @Input() personList: Array<PersonModel> = [];
 
+  deletePerson(index: number): void {
+    this.personList.splice(index, 1);
+  }
+  editPerson(index: number): void {
+    const person = this.personList[index];
+  }
 }
