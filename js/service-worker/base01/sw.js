75b563a81da64a6fcdd1d75a7917b2be5e4395a1
@Julnarot <rauljhonatan.tola@gmail.com>
2024-04-10 12:41:17 -0500

    course(js/ws): injecting custom stylesheet on  response

--- js/service-worker/base01/sw.js
+++ js/service-worker/base01/sw.js
@@ -1,7 +1,18 @@
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.includes('style')) {
-    event.respondWith(null);
-  } else {
-    event.respondWith(fetch(event.request));
+  if (event.request.url.includes('style.css')) {
+    const injectedCssResponse = new Response(
+      `
+    body {
+      background-color: red;
+      color: white;
+    }
+    `,
+      {
+        headers: {
+          'Content-Type': 'text/css',
+        },
+      }
+    );
+    event.respondWith(injectedCssResponse);
   }
 });
