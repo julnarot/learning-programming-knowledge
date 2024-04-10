03b7db0308c57c92d397bbe677ce7bf1b544c84b
@Julnarot <rauljhonatan.tola@gmail.com>
2024-04-10 12:33:41 -0500

    course(js/fetch): respond null for style

--- js/service-worker/base01/sw.js
+++ js/service-worker/base01/sw.js
@@ -1,7 +1,6 @@
 self.addEventListener('fetch', (event) => {
-  console.log(event.request.url)
   if (event.request.url.includes('style')) {
-    event.respondWith(null)
+    event.respondWith(null);
   } else {
     event.respondWith(fetch(event.request));
   }
