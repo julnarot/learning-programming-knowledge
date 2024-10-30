bf9b7b66e38325c0c765265c048369f236ae0956
julnarot <rauljhonatan.tola@gmail.com>
2024-10-29 21:02:18 -0500

    feat: now can show profile data from DB

--- node/basic-cv/app.js
+++ node/basic-cv/app.js
@@ -6,45 +6,21 @@ const app = express();
 const client = new Client();
 
 const myProfileData = {
+    username: "@Julnarot",
     email: "rauljhonatan.tola@gmail.com",
     mainSlogan: 'The more transparent the writing, the more visible the poetry.',
-    mainMessage: "Hello!! I'm Raul Jonatan, and i've been development web solutions alternatives  of multiple enterprise sectors for almost 8 years",
+    mainMessage: "At the beginning of 16 I decided to take on this exciting challenge of building tools that allow for easy access to information effectively for the person or people who require it through the use of the art of writing computer code.",
     social: [
         'https://github.com/julnarot',
         'https://pe.linkedin.com/in/julnarot'
     ],
     about: "Bachelor's degree in Systems Engineering with recognized work experience in the process of analysis, development, implementation, migration and maintenance of web software in various projects in the commercial, educational, educational-financial, energy-fuels and public administration fields.",
     photoUrl: 'https://contents.bebee.com/users/id/rijYy65368c4be7f6d/_avatar-ywr7p-400.png',
-}
+};
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-
-app.get('/', async (req, res) => {
-    await connectDB();
-    let result = [];
-    try {
-        const resp = await client.query('SELECT * FROM dual');
-        if (resp) {
-            this.result = resp.rows;
-            console.log(resp.rows)
-        }
-    } catch (err) {
-        console.error('Error executing query:', err);
-    } finally {
-        await disconnectDB();
-    }
-    console.log(result);
-
-    res.render('index', myProfileData)
-});
-
-app.listen(3000, () => {
-    console.log('Running on: http://localhost:3000')
-})
-
 
 const connectDB = async () => {
     try {
@@ -55,6 +31,19 @@ const connectDB = async () => {
     }
 };
 
+app.get('/', async (req, res) => {
+    let result = {};
+    try {
+        const resp = await client.query("SELECT * FROM profiles where username = 'julnarot'");
+        if (resp) {
+            result = resp.rows.pop(); 
+        }
+    } catch (err) {
+        console.error('Error executing query:', err);
+    }
+    res.render('index', { ...myProfileData, ...result });
+});
+
 const disconnectDB = async () => {
     try {
         await client.end();
@@ -63,3 +52,13 @@ const disconnectDB = async () => {
         console.error('Error closing connection:', err);
     }
 };
+
+app.listen(3000, async () => {
+    await connectDB();
+    console.log('Running on: http://localhost:3000');
+});
+
+process.on('SIGINT', async () => {
+    await disconnectDB();
+    process.exit(0);
+});
